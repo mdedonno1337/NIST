32c9207d1bee8b37714d771e7c9dfb4ee7b63b34
Marco De Donno <Marco.DeDonno@unil.ch>
2016-06-11 16:05:16 +0200

    Add debug information

--- __init__.py
+++ __init__.py
@@ -1,6 +1,58 @@
 #!/usr/bin/env python
 
 from _collections import defaultdict
+import logging
+
+
+class MyLogger:
+    def __init__( self ):
+        self.mode = logging.CRITICAL
+        self.format = '%(asctime)s -- %(levelname)s -- %(message)s'
+        logging.basicConfig( level = self.mode, format = self.format )
+        self.log = logging.getLogger( 'NIST library' )
+    
+    def setMode( self, mode ):
+        mode = mode.lower()
+        
+        if mode == 'debug':
+            self.mode = logging.DEBUG
+        elif mode == 'info':
+            self.mode = logging.INFO
+        elif mode == 'warning':
+            self.mode = logging.WARNING
+        elif mode == 'error':
+            self.mode = logging.ERROR
+        elif mode == 'critical':
+            self.mode = logging.CRITICAL
+        else:
+            raise Exception( 'debug level unknown : %s' % mode )
+        
+        self.log.setLevel( self.mode )
+    
+    def leveler( self, msg, level ):
+        return '\t' * level + msg
+    
+    def debug( self, msg, level = 0 ):
+        msg = self.leveler( msg, level )
+        self.log.debug( msg )
+    
+    def info( self, msg, level = 0 ):
+        msg = self.leveler( msg, level )
+        self.log.info( msg )
+    
+    def warning( self, msg, level = 0 ):
+        msg = self.leveler( msg, level )
+        self.log.warning( msg )
+        
+    def error( self, msg, level = 0 ):
+        msg = self.leveler( msg, level )
+        self.log.error( msg )
+    
+    def critical( self, msg, level = 0 ):
+        msg = self.leveler( msg, level )
+        self.log.critical( msg )
+    
+debug = MyLogger()
 
 ################################################################################
 # 
@@ -28,12 +80,16 @@ DO = '.'
 
 class NIST:
     def __init__( self ):
+        debug.info( 'initialization of the NIST object' )
+        
         self.filename = None
         self.data = defaultdict( dict )
         
         return
     
     def loadFromFile( self, infile ):
+        debug.info( 'Reading from file : %s' % infile )
+        
         self.filename = infile
     
         with open( infile, "rb" ) as fp:
@@ -42,12 +98,16 @@ class NIST:
         self.load( data )
     
     def load( self, data ):
+        debug.info( 'Loading object' )
+        
         records = data.split( FS )
         expected = []
         
         ########################################################################
         #    NIST Type01
         ########################################################################
+        
+        debug.info( 'Type01 parsing', 1 )
         
         t01 = records[0].split( GS )
         record01 = {}
@@ -71,7 +131,10 @@ class NIST:
         #    NIST Type02 and after
         ########################################################################
         
+        debug.debug( 'Expected Types : %s' % ', '.join( [ str( ntype ) for ntype, _ in expected[ 1: ] ] ), 1 )
+        
         for ntype, _ in expected[ 1: ]:
+            debug.info( 'Type%02d parsing' % ntype, 1 )
             LEN = 0
             
             if ntype in [ 2, 9, 13 ]:
@@ -102,10 +165,12 @@ class NIST:
                         offset += len( tag ) + 1
                         
                         value = data[ offset : end ]
+                        debug.debug( "%d.%03d:\t%s" % ( ntype, tagid, bindump( value ) ), 2 )
                         recordx[ tagid ] = value
                         data = data[ end: ]
                         break
                         
+                    debug.debug( "%d.%03d:\t%s" % ( ntype, tagid, value ), 2 )
                     recordx[ tagid ] = value
                     offset += len( t ) + 1
                     
