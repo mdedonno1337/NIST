54fe6ef23b1a493f52a5def04157620ec87d3fc5
Marco De Donno <Marco.DeDonno@unil.ch>
2016-06-11 22:39:51 +0200

    Add the labels for the Type01, Type02, Type09 and Type13 (abbreviation and full label)

--- __init__.py
+++ __init__.py
@@ -67,6 +67,129 @@ def bindump( data ):
 ################################################################################
 # 
 ################################################################################
+
+LABEL = {
+    1: {
+        1: "LEN",
+        2: "VER",
+        3: "CNT",
+        4: "TOT",
+        5: "DAT",
+        6: "PRY",
+        7: "DAI",
+        8: "ORI",
+        9: "TCN",
+        10: "TCR",
+        11: "NSR",
+        12: "NTR",
+        13: "DOM",
+        14: "GMT",
+        15: "DCS",
+    },
+    2: {
+        1: "LEN",
+        2: "IDC",
+        3: "SYS",
+    },
+    9: {
+        1: "LEN",
+        2: "IDC",
+        3: "IMP",
+        4: "FMT",
+        128: "HLL",
+        129: "VLL",
+        130: "SLC",
+        131: "HPS",
+        132: "VPS",
+        134: "FGP",
+        999: "DAT",
+    },
+    13: {
+        1: "LEN",
+        2: "IDC",
+        3: "IMP",
+        4: "SRC",
+        5: "LCD",
+        6: "HLL",
+        7: "VLL",
+        8: "SLC",
+        9: "HPS",
+        10: "VPS",
+        11: "GCA",
+        12: "BPX",
+        13: "FGP",
+        14: "SPD",
+        15: "PPC",
+        20: "COM",
+        24: "LQM",
+        999: "DAT",
+    }
+}
+
+FULLLABEL = {
+    1: {
+        1: "Logical record length",
+        2: "Version number",
+        3: "File content",
+        4: "Type of transaction",
+        5: "Date",
+        6: "Priority",
+        7: "Destination agency identifier",
+        8: "Originating agency identifier",
+        9: "Transaction control number",
+        10: "Transaction control reference",
+        11: "Native scanning resolution",
+        12: "Nominal transmitting resolution",
+        13: "Domain name",
+        14: "Greenwich mean time",
+        15: "Directory of character sets"
+    },
+    2: {
+        1: "Logical record length",
+        2: "Image designation character"
+    },
+    9: {
+        1: "Logical record length",
+        2: "Image designation character",
+        3: "Impression type",
+        4: "Minutiae format",
+        5: "Originating fingerprint reading system",
+        6: "Finger position",
+        7: "Fingerprint pattern classification",
+        8: "Core position",
+        9: "Delta(s) position",
+        10: "Number of minutiae",
+        11: "Minutiae ridge count indicator",
+        12: "Minutiae and ridge count data"
+    },
+    13: {
+        1: "Logical record length",
+        2: "Image designation character",
+        3: "Impression type",
+        4: "Source agency / ORI",
+        5: "Latent capture date",
+        6: "Horizontal line length",
+        7: "Vertical line length",
+        8: "Scale units",
+        9: "Scale units",
+        10: "Scale units",
+        11: "Compression algorithm",
+        12: "Bits per pixel",
+        13: "Finger / palm position",
+        14: "Search Position Descriptors",
+        15: "Print Position Coordinates",
+        16: "Scanned horizontal pixel scale",
+        17: "Scanned vertical pixel scale",
+        20: "Comment",
+        24: "Latent quality metric",
+        999: "Image data",
+    }
+}
+
+################################################################################
+# 
+################################################################################
+
 FS = chr( 28 )
 GS = chr( 29 )
 RS = chr( 30 )
