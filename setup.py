7eb021362c3bf6bc148cdb0984ad49f8e4728317
Marco De Donno <Marco.DeDonno@unil.ch>
2016-06-20 00:31:43 +0200

    First version of the setup.py files.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from distutils.core import setup
+
+setup( 
+    name = 'NIST',
+    version = "0.10.0",
+    description = 'Python library for manipulating NIST files (Data Format for the Interchange of Fingerprint, Facial & Other Biometric Information)',
+    author = 'Marco De Donno',
+    author_email = 'Marco.DeDonno@unil.ch; mdedonno1337@gmail.com',
+    packages = [ 'NIST' ],
+ )
