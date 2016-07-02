264e7903965f3f328ef2d67d60e147a1b7e3d92e
Marco De Donno <Marco.DeDonno@unil.ch>
2016-07-03 01:44:45 +0200

    Function to generate the tryptych (latent, latent annotated and quality map)

--- NIST/ULWLQMetric/__init__.py
+++ NIST/ULWLQMetric/__init__.py
@@ -2,6 +2,7 @@
 #  *-* coding: cp850 *-*
 
 from future.builtins import super
+from PIL import Image
 
 from MDmisc.boxer import boxer
 from MDmisc.logger import debug
@@ -24,6 +25,21 @@ try:
             idc = self.checkIDC( 9, idc )
             self.data[ 9 ][ idc ].update( super().ULWLQMetric_encode( 'EFS' ) )
             self.clean()
+        
+        def get_latent_triptych( self, idc = -1 ):
+            img = self.get_latent( 'PIL', idc )
+            anno = self.get_latent_annotated( idc )
+            qmap = super().ULWLQMetric_encode( "image" )
+            
+            qmap = qmap.resize( img.size, Image.ANTIALIAS )
+            
+            new = Image.new( "RGB", ( img.size[ 0 ] * 3, img.size[ 1 ] ), "white" )
+            
+            new.paste( img, ( 0, 0 ) )
+            new.paste( anno, ( img.size[ 0 ], 0 ) )
+            new.paste( qmap, ( img.size[ 0 ] * 2, 0 ) )
+            
+            return new
             
 except:
     debug.critical( boxer( "ULWLQMetric module not found", "Have you installed the ULWLQMetric library?" ) )
