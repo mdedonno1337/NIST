9186637d22fc6b5b6a2745dc0d2ca363938bc0e5
Marco De Donno <Marco.DeDonno@unil.ch>
2016-11-08 18:59:25 +0100

    Add the possibility to filter out the fileds in the 'get_minutiae_paired()' function

--- NIST/MDD/__init__.py
+++ NIST/MDD/__init__.py
@@ -24,9 +24,27 @@ class NIST_MDD( NISTf ):
         """
         return split_r( [ RS, US ], self.get_field( "9.255", idc ) )
     
-    def get_minutiae_paired( self, idc = -1 ):
+    def get_minutiae_paired( self, format = None, idc = -1 ):
         """
             Return all minutiae which are paired. The pairing information is not
             returned here.
+            
+                >>> mark.get_minutiae_paired()
+                [['1', 7.85, 7.05, 290, '0', 'A'], ['2', 13.8, 15.3, 155, '0', 'A'], ['3', 11.46, 22.32, 224, '0', 'A']]
+            
+            It is also possible to filter out the interesting fields:
+            
+                >>> mark.get_minutiae_paired( 'xy' )
+                [[7.85, 7.05], [13.8, 15.3], [11.46, 22.32]]
         """
-        return [ self.get_minutia_by_id( minutiaid, idc ) for minutiaid, _ in self.get_pairing( idc ) ]
+        if type( format ) == int:
+            idc, format = format, self.minutiaeformat
+        
+        elif format == None:
+            format = self.minutiaeformat
+        
+        try:
+            return [ self.get_minutia_by_id( minutiaid, format, idc ) for minutiaid, _ in self.get_pairing( idc ) ]
+         
+        except TypeError:
+            return None
