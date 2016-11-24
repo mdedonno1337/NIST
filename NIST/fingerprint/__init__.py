056e6aed65841b1ff588428cc63d9198ba304aca
Marco De Donno <Marco.DeDonno@unil.ch>
2016-11-24 18:33:17 +0100

    Doc update

--- NIST/fingerprint/__init__.py
+++ NIST/fingerprint/__init__.py
@@ -128,8 +128,19 @@ class NISTf( NIST ):
             
             To get all information, dont speficy any format:
             
-                >>> mark.get_minutiae()
-                [Minutiae( i='1', x='7.85', y='7.05', t='290', q='0', d='A' ), Minutiae( i='2', x='13.8', y='15.3', t='155', q='0', d='A' ), Minutiae( i='3', x='11.46', y='22.32', t='224', q='0', d='A' ), Minutiae( i='4', x='22.61', y='25.17', t='194', q='0', d='A' ), Minutiae( i='5', x='6.97', y='8.48', t='153', q='0', d='A' ), Minutiae( i='6', x='12.58', y='19.88', t='346', q='0', d='A' ), Minutiae( i='7', x='19.69', y='19.8', t='111', q='0', d='A' ), Minutiae( i='8', x='12.31', y='3.87', t='147', q='0', d='A' ), Minutiae( i='9', x='13.88', y='14.29', t='330', q='0', d='A' ), Minutiae( i='10', x='15.47', y='22.49', t='271', q='0', d='A' )]
+                >>> mark.get_minutiae() # doctest: +NORMALIZE_WHITESPACE
+                [
+                    Minutiae( i='1', x='7.85', y='7.05', t='290', q='0', d='A' ),
+                    Minutiae( i='2', x='13.8', y='15.3', t='155', q='0', d='A' ),
+                    Minutiae( i='3', x='11.46', y='22.32', t='224', q='0', d='A' ),
+                    Minutiae( i='4', x='22.61', y='25.17', t='194', q='0', d='A' ),
+                    Minutiae( i='5', x='6.97', y='8.48', t='153', q='0', d='A' ),
+                    Minutiae( i='6', x='12.58', y='19.88', t='346', q='0', d='A' ),
+                    Minutiae( i='7', x='19.69', y='19.8', t='111', q='0', d='A' ),
+                    Minutiae( i='8', x='12.31', y='3.87', t='147', q='0', d='A' ),
+                    Minutiae( i='9', x='13.88', y='14.29', t='330', q='0', d='A' ),
+                    Minutiae( i='10', x='15.47', y='22.49', t='271', q='0', d='A' )
+                ]
                 >>> [ m.as_list() for m in mark.get_minutiae() ]
                 [['1', 7.85, 7.05, 290, '0', 'A'], ['2', 13.8, 15.3, 155, '0', 'A'], ['3', 11.46, 22.32, 224, '0', 'A'], ['4', 22.61, 25.17, 194, '0', 'A'], ['5', 6.97, 8.48, 153, '0', 'A'], ['6', 12.58, 19.88, 346, '0', 'A'], ['7', 19.69, 19.8, 111, '0', 'A'], ['8', 12.31, 3.87, 147, '0', 'A'], ['9', 13.88, 14.29, 330, '0', 'A'], ['10', 15.47, 22.49, 271, '0', 'A']]
             
@@ -173,9 +184,19 @@ class NISTf( NIST ):
             the NIST object, i.e. the finger is missing, an empty list of
             minutiae is returned, to complete the tenprint card.
             
-            >>> pr.get_minutiae_all()
-            [[Minutiae( i='1', x='7.85', y='7.05', t='290', q='0', d='A' ), Minutiae( i='2', x='13.8', y='15.3', t='155', q='0', d='A' ), Minutiae( i='3', x='11.46', y='22.32', t='224', q='0', d='A' ), Minutiae( i='4', x='22.61', y='25.17', t='194', q='0', d='A' ), Minutiae( i='5', x='6.97', y='8.48', t='153', q='0', d='A' ), Minutiae( i='6', x='12.58', y='19.88', t='346', q='0', d='A' ), Minutiae( i='7', x='19.69', y='19.8', t='111', q='0', d='A' ), Minutiae( i='8', x='12.31', y='3.87', t='147', q='0', d='A' ), Minutiae( i='9', x='13.88', y='14.29', t='330', q='0', d='A' ), Minutiae( i='10', x='15.47', y='22.49', t='271', q='0', d='A' )], [], [], [], [], [], [], [], [], []]
-            
+            >>> pr.get_minutiae_all() # doctest: +NORMALIZE_WHITESPACE
+                [[
+                    Minutiae( i='1', x='7.85', y='7.05', t='290', q='0', d='A' ),
+                    Minutiae( i='2', x='13.8', y='15.3', t='155', q='0', d='A' ),
+                    Minutiae( i='3', x='11.46', y='22.32', t='224', q='0', d='A' ),
+                    Minutiae( i='4', x='22.61', y='25.17', t='194', q='0', d='A' ),
+                    Minutiae( i='5', x='6.97', y='8.48', t='153', q='0', d='A' ),
+                    Minutiae( i='6', x='12.58', y='19.88', t='346', q='0', d='A' ),
+                    Minutiae( i='7', x='19.69', y='19.8', t='111', q='0', d='A' ),
+                    Minutiae( i='8', x='12.31', y='3.87', t='147', q='0', d='A' ),
+                    Minutiae( i='9', x='13.88', y='14.29', t='330', q='0', d='A' ),
+                    Minutiae( i='10', x='15.47', y='22.49', t='271', q='0', d='A' )
+                ], [], [], [], [], [], [], [], [], []]
         """
         if ifany( [ 4, 14 ], self.get_ntype() ):
             if format == None:
