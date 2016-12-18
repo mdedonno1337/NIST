cce50788231ec792cc3b49be5582972b805cb1d6
Marco De Donno <Marco.DeDonno@unil.ch>
2016-12-18 23:26:46 +0100

    Add the version number from git in the doc

--- doc/conf.py
+++ doc/conf.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+import os
+import sys
+
+sys.path.insert( 0, os.path.abspath( "./" ) )
+
+################################################################################
+# 
+################################################################################
+
 project = u'NIST'
 copyright = u'2016, Marco De Donno'
 author = u'Marco De Donno'
 
-version = u'dev'
-release = u'dev'
+from version import __version__
+
+version = __version__
+release = __version__
 
 ################################################################################
 
