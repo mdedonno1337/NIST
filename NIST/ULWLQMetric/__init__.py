fd3f13cb101c3402c326daae5c6795d881470e92
Marco De Donno <Marco.DeDonno@unil.ch>
2017-03-02 14:56:00 +0100

    Add the NISTULWLQMetric.get_minutiae() function

--- NIST/ULWLQMetric/__init__.py
+++ NIST/ULWLQMetric/__init__.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+from __future__ import division
+
 from future.builtins import super
 
 from MDmisc.boxer import boxer
 from MDmisc.logger import debug
+from MDmisc.string import split_r
 from SoftPillow import Image
     
 from ..fingerprint import NISTf
+from ..fingerprint import AnnotationList, Minutia
+from ..traditional import RS, US
     
 try:
     from ULWLQMetric import ULWLQMetric
@@ -29,6 +34,39 @@ try:
             idc = self.checkIDC( 9, idc )
             self.data[ 9 ][ idc ].update( super().ULWLQMetric_encode( 'EFS' ) )
             self.clean()
+        
+        def get_minutiae( self, format = None, idc = -1 ):
+            """
+                Overload of the `NIST.fingerprint.NISTf.get_minutiae()` function
+                to extract the information from the 9.331 field if not present
+                in the 9.012 field.
+                
+                .. see:: :func:`NIST.fingerprint.NISTf.get_minutiae()`
+            """
+            try:
+                return super().get_minutiae( format = format, idc = idc )
+            
+            except AttributeError:
+                lst = AnnotationList()
+                
+                for m in split_r( [ RS, US ], self.get_field( "9.331", idc ) ):
+                    if m == [ '' ]:
+                        break
+                    
+                    else:
+                        x, y, theta, d, dr, dt = m
+                        
+                        x = int( x ) / 100
+                        y = int( y ) / 100
+                        y = ( self.get_height( idc ) / self.get_resolution( idc ) * 25.4 ) - y
+                        theta = ( int( theta ) + 180 ) % 360 
+                        
+                        dr = int( dr )
+                        dt = int( dt )
+                        
+                        lst.append( Minutia( [ x, y, theta, d, dr, dt ], format = "xytdab" ) )
+                
+                return lst
         
         def get_latent_triptych( self, content = "quality", idc = -1 ):
             """
