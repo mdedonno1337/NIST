e8fd29b58402f8667ac71dcc1a8817f9cdd40a57
Marco De Donno <Marco.DeDonno@unil.ch>
2017-06-12 16:08:30 +0200

    Add the AnnotatinList.get_n_closest_from_point() function

--- NIST/fingerprint/functions.py
+++ NIST/fingerprint/functions.py
@@ -991,6 +991,20 @@ class AnnotationList( eobject ):
         """
         self._data.sort( key = lambda m: m >> p )
     
+    def get_n_closest_from_point( self, n, p ):
+        """
+            Return the n closest Annotations from a particular point p.
+            
+            :param n: Number of points to return
+            :type n: int
+            
+            :param p: Point
+            :type p: Annotation
+        """
+        tmp = self.get()
+        tmp.sort_dist_point( p )
+        return tmp[ 0 : n ]
+    
     ############################################################################
     
     def __str__( self ):
